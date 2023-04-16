b31b3e30126ecf8fbb67c760f7c6501ecccaed89
domscript <vladimirndsm@gmail.com>
2023-04-16 21:06:13 +0700

    fix

---
+++ src/components/MDX/JS/ClassData.tsx
@@ -0,0 +1,13 @@
+export function ClassData() {
+  return (
+    <>
+      <h2>
+        <span
+          className="text-cyan-300 hover:opacity-50"
+          title="SPECIAL KEYWORD defines a generator function expression">
+          Class
+        </span>
+      </h2>
+    </>
+  );
+}
